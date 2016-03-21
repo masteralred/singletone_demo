4ff141e3a6e97c86427077afe0093847f0c4ccde
Alexander Zhadnov <masteralred@gmail.com>
2016-03-21 11:48:09 +0300

    Add simple log to file function

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ class Logger
   def self.say_something
     puts "haha"
   end
+
+  def self.log_something what
+    file = File.open 'log.txt', 'a'
+    file.puts what
+    file.close
+  end
 end
 
 Logger.say_something
+Logger.log_something 'wazzup'
